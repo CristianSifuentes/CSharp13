8d0b3d851b5e25907ec98b66c8f8da94f4f6eb0b
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-05-07 14:54:48 -0600

    Starting with C# 13 Features

---
+++ CSharp13FeaturesDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿﻿using static System.Console;
+
+
+namespace CSharp13FeaturesDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WriteLine("C# 13.0 Features Demo");
+    }
+}
